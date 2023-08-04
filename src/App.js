ca0236ffd87a1025f185548c9744c8922b798423
yongsung <mirr001003@hanyang.ac.kr>
2023-08-04 22:17:31 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import TodoList from './components/TodoList';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 function App() {
+  const filters=['all','active','completed'];
+  const [filter, setFilter] = useState(filters[0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DarkModeProvider>
+    <Header 
+      filters={filters} 
+      filter={filter} 
+      onFilterChange={setFilter}/>
+    <TodoList filter={filter}/>
+      
+    </DarkModeProvider>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Header.module.css'
+import { useDarkMode } from '../../context/DarkModeContext';
+import {HiMoon, HiSun} from 'react-icons/hi';
+export default function Header({filters,filter,onFilterChange}) {
+    const {darkMode, toggleDarkMode} = useDarkMode();
+    return (
+        <header className={styles.header}>
+            <button className={styles.toggle} onClick={toggleDarkMode}>
+                {!darkMode && <HiMoon/>}
+                {darkMode && <HiSun/>}
+            </button>
+            <ul className={styles.filters}>
+                {filters.map((value, index) => 
+                <li key={index} >
+                    <button 
+                        className={`${styles.filter} ${filter === value && styles.selected} `}
+                        onClick={() => onFilterChange(value)}>{value}</button>
+                </li>)}
+            </ul>
+        </header>
+    );
+}
+
